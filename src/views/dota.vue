e2c388d95ccd25b5817885cfedda439409ce3a75
Evgenii Petrovich <evgeniipetrovich@MacBook-Pro-Evgenii.local>
2019-10-03 18:34:43 +0300

    dota done

--- src/views/dota.vue
+++ src/views/dota.vue
@@ -6,5 +6,8 @@
       <p>А подготовка к соревнованиям будет ввестись с тренерами
         из академии <a href="https://vk.com/cyberiumacademy">Cyberium</a></p>
     </div>
+    <div class="form">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeCgvorStEOJRJq5xQfQXJIRR5e3amaR39LDdf_ILzsy8ElnA/viewform?embedded=true" width="640" height="1686" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
+    </div>
   </div>
 </template>
